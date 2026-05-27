decf43823fa4d4eeb3d4a67fe73fb6918f586821
nmemmert <nmemmert@users.noreply.github.com>
2026-05-27 09:41:53 -0400

    allowed server

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['study.necloud.us'],
+  },
   test: {
     environment: 'jsdom',
     globals: true,
